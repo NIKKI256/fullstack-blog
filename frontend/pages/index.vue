269e29c4ddf64885bdfefe705be7f0037b70b120
Nikita Borovkov <m.borovkov@trementum.net>
2023-05-10 16:32:39 +0300

    init

---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <auth-login v-if="isLogin" @goToRegistration="toggleAuth"/>
+    <auth-registration v-else @goToLogin="toggleAuth"/>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isLogin = ref(true)
+
+const toggleAuth = () => {
+  isLogin.value = !isLogin.value
+}
+</script>
