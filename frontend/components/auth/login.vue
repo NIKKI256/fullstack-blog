2da0b5f2af49197db0feb815e89ce522c798802f
Nekit Borov <nekitborov@gmail.com>
2023-05-19 14:57:19 +0300

    rewrite auth components

--- frontend/components/auth/login.vue
+++ frontend/components/auth/login.vue
@@ -10,24 +10,18 @@
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
-            <input v-model="email" id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input v-model="formData.email" id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
         <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-<!--            <div class="text-sm">-->
-<!--              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>-->
-<!--            </div>-->
-          </div>
           <div class="mt-2">
-            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input v-model="formData.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
         <div>
-          <button @click="login" :disabled="checkPrimitiveArgs(email, password)" type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-75 disabled:cursor-not-allowed disabled:hover:bg-indigo-600">Sign in</button>
+          <button @click="login" :disabled="checkPrimitiveArgs(formData.email, formData.password)" type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-75 disabled:cursor-not-allowed disabled:hover:bg-indigo-600">Sign in</button>
         </div>
       </div>
 
@@ -39,23 +33,21 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { reactive } from 'vue'
+import { useCheckEmptyArgs } from '@/composables/checkEmptyArgs'
 import nuxtStorage from 'nuxt-storage'
-import { useCheckEmptyArgs } from '../../composables/checkEmptyArgs'
 
 const { $post } = useNuxtApp()
 const { checkPrimitiveArgs } = useCheckEmptyArgs()
 
-const email = ref('')
-const password = ref('')
+const formData = reactive({
+  email: '',
+  password: ''
+})
 
 const login = async () => {
   try {
-    const payload = {
-      email: email.value,
-      password: password.value
-    }
-    const { token } = await $post('/apiUsers/login', payload)
+    const { token } = await $post('/apiUsers/login', formData)
     nuxtStorage.localStorage.setData('token', token, 24, 'h')
   } catch (e) {
     const { message } = e.data
--- frontend/components/auth/registration.vue
+++ frontend/components/auth/registration.vue
@@ -10,14 +10,14 @@
         <div>
           <label for="login" class="block text-sm font-medium leading-6 text-gray-900">Login</label>
           <div class="mt-2">
-            <input v-model="login" id="login" name="login" type="text" autocomplete="login" required="" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input v-model="formData.login" id="login" name="login" type="text" autocomplete="login" required="" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
-            <input v-model="email" id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input v-model="formData.email" id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
@@ -29,12 +29,12 @@
             <!--            </div>-->
           </div>
           <div class="mt-2">
-            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input v-model="formData.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
         <div>
-          <button @click="registration" type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+          <button @click="registration" :disabled="checkPrimitiveArgs(formData.email, formData.password)" type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
         </div>
       </div>
 
@@ -46,25 +46,24 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { reactive } from 'vue'
+import { useCheckEmptyArgs } from '@/composables/checkEmptyArgs'
 
 const { $post } = useNuxtApp()
+const { checkPrimitiveArgs } = useCheckEmptyArgs()
 
-const login = ref('')
-const email = ref('')
-const password = ref('')
+const formData = reactive({
+  login: '',
+  email: '',
+  password: ''
+})
 
 const registration = async () => {
   try {
-    const payload = {
-      login: login.value,
-      email: email.value,
-      password: password.value
-    }
-    const { data } = await $post('/apiUsers/registration', payload)
-    console.log(data)
+    await $post('/apiUsers/registration', formData)
   } catch (e) {
-    console.log(e)
+    const { message } = e.data
+    console.error(message)
   }
 }
 </script>
